2742d796bdcfbd8a97bac236acda4109f490cff0
kee cha <kee_cha18@yahoo.com>
2020-01-06 16:12:40 -0600

    added customer model and employee model. Finish the migration process

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PickupDay { get; set; }
+        public int ZipCode { get; set; }
+        public double Balance { get; set; }
+        public bool PickupConfirmation { get; set; }
+        public DateTime SuspendStart { get; set; }
+        public DateTime SuspendEnd { get; set; }
+        public DateTime ExtraPickupDate { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ZipCode { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
